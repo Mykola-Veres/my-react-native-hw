1d13232555c3fa449ff91e9dbc98dc6857ff7b47
Mykola-Veres <veresnickolay@gmail.com>
2022-09-12 00:40:05 +0300

    add

--- screens/mainScreen/CreateScreen.jsx
+++ screens/mainScreen/CreateScreen.jsx
@@ -1,15 +1,99 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { useState, useEffect } from "react";
 
-const CreateScreen = () => {
+const CreateScreen = ({ navigation }) => {
+  const [camera, setCamera] = useState(null);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [type, setType] = useState(CameraType.back);
+  const [photo, setPhoto] = useState(null);
+  console.log("camera", camera);
+
+  if (!permission) {
+    return <View />;
+  }
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+  async function takePicture() {
+    // setType((current) =>
+    //   current === CameraType.back ? CameraType.front : CameraType.back
+    // );
+    const photo = await camera.takePictureAsync();
+    console.log("photo", photo);
+    setPhoto(photo.uri);
+  }
+
+  const sendPhoto = () => {
+    console.log("navigation", navigation);
+    navigation.navigate("Posts", { photo });
+  };
+
   return (
     <View style={styles.conreiner}>
-      <Text>CreateScreen</Text>
+      <Camera style={styles.camera} type={type} ref={setCamera}>
+        {photo && (
+          <View style={styles.takePhotoContainer}>
+            <Image
+              source={{ uri: photo }}
+              style={{ height: 200, width: 200, borderRadius: 10 }}
+            />
+          </View>
+        )}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={takePicture}>
+            <Text style={styles.text}>Click Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <View style={styles.sendPhotoContainer}>
+        <TouchableOpacity
+          style={{ ...styles.button, backgroundColor: "#ffe4c4" }}
+          onPress={sendPhoto}
+        >
+          <Text style={styles.text}>Send photo</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  conreiner: { flex: 1, justifyContent: "center", alignItems: "center" },
+  conreiner: { flex: 1 },
+  camera: { alignItems: "center", height: 300, marginTop: 50 },
+  buttonContainer: {
+    marginTop: 200,
+  },
+  button: {
+    borderWidth: 2,
+    borderColor: "#d2691e",
+    borderRadius: 50,
+    width: 70,
+    height: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: { color: "#d2691e" },
+  takePhotoContainer: {
+    position: "absolute",
+    top: 50,
+    left: 10,
+    borderColor: "#fff",
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  sendPhotoContainer: {
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default CreateScreen;
--- screens/mainScreen/PostScreen.jsx
+++ screens/mainScreen/PostScreen.jsx
@@ -1,15 +1,44 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 
-const PostScreen = ({ navigation }) => {
+const PostScreen = ({ route }) => {
+  const [posts, setPosts] = useState([]);
+  console.log("route.params", route.params);
+
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
+  console.log("posts", posts);
+
   return (
     <View style={styles.conreiner}>
-      <Text>PostScreen</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, indx) => indx.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              marginBottom: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text>Photo</Text>
+            <Image
+              source={{ uri: item.photo }}
+              style={{ width: 350, height: 200 }}
+            />
+          </View>
+        )}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  conreiner: { flex: 1, justifyContent: "center", alignItems: "center" },
+  conreiner: { flex: 1 },
 });
 
 export default PostScreen;

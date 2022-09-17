37976d631ee6473f1abe4a7d5a2204011e4c83ab
Mykola-Veres <veresnickolay@gmail.com>
2022-09-17 23:52:03 +0300

    add

--- redux/store.jsx
+++ redux/store.jsx
@@ -1,6 +1,9 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { authSlice } from "./auth/authReducer";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  [authSlice.name]: authSlice.reducer,
+});
 
 export const store = configureStore({
   reducer: rootReducer,
